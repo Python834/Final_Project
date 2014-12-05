08ba5532536e1c329530e02bf2afdbf27ab68988
Patrick McManis <pytho834@gmail.com>
2014-12-04 21:01:47 -0800

    First Commit

--- www/js/pizza_ui.js
+++ www/js/pizza_ui.js
@@ -11,4 +11,27 @@ $( "#sortable" ).sortable();
 $('#orderLink').on('click', function(event){
     event.preventDefault();
     Navigator.vibrate(5000);
+    showAlert()
 });
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+//
+function onDeviceReady() {
+    // Empty
+}
+
+// alert dialog dismissed
+function alertDismissed() {
+    // do something
+}
+
+function showAlert() {
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        alertDismissed,         // callback
+        'Game Over',            // title
+        'Done'                  // buttonName
+    );
+}
